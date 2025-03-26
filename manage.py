4568092a827d74183610a17815c46a76d5921976
EC2 Default User <ec2-user@ip-172-31-81-192.ec2.internal>
2025-03-26 00:08:03 +0000

    manage file changes

--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_suggestions.settings')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scalable_new.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_suggestions.settings')
+    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scalable_new.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
