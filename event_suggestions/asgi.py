f9e2c93cf87e3d42b4ca8cbe7dcc6c02a0ce5164
EC2 Default User <ec2-user@ip-172-31-81-192.ec2.internal>
2025-03-30 22:50:08 +0000

    wsgi

--- event_suggestions/asgi.py
+++ event_suggestions/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reminder_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_suggestions.settings')
 
 application = get_asgi_application()
